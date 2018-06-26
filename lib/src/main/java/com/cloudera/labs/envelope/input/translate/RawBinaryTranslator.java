e7c67d5c4a23e931b1158af90cc6930e8d5562ed
Jeremy Beard <jeremy@cloudera.com>
2018-06-26 08:55:43 -0400

    [ENV-205] Add translators for passing raw values straight through (#189)
    * Add translators for passing raw values straight through
    
    * Add service loader references

---
+++ lib/src/main/java/com/cloudera/labs/envelope/input/translate/RawBinaryTranslator.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed to Cloudera, Inc. under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Cloudera, Inc. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloudera.labs.envelope.input.translate;
+
+import com.cloudera.labs.envelope.load.ProvidesAlias;
+import com.cloudera.labs.envelope.utils.RowUtils;
+import com.google.common.collect.Lists;
+import com.typesafe.config.Config;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.Collections;
+
+public class RawBinaryTranslator implements Translator<byte[], byte[]>, ProvidesAlias {
+
+  @Override
+  public void configure(Config config) { }
+
+  @Override
+  public Iterable<Row> translate(byte[] key, byte[] value) throws Exception {
+    return Collections.singleton(RowFactory.create(key, value));
+  }
+
+  @Override
+  public StructType getSchema() {
+    return RowUtils.structTypeFor(
+        Lists.newArrayList("key", "value"),
+        Lists.newArrayList("binary", "binary")
+    );
+  }
+
+  @Override
+  public String getAlias() {
+    return "rawbinary";
+  }
+
+}
