974fd90b6bb81b273dd100e143677ea156dac092
Chris Ingrassia <cingrassia@mediamath.com>
2018-06-26 10:36:08 -0400

    Build against Spark 2.3.0-cloudera2 and Kudu 1.6.0 Updates KuduServiceCredentialProvider to use newer interface

--- external/kudu/src/main/java/com/cloudera/labs/envelope/kudu/KuduServiceCredentialProvider.java
+++ external/kudu/src/main/java/com/cloudera/labs/envelope/kudu/KuduServiceCredentialProvider.java
@@ -47,7 +47,7 @@ public class KuduServiceCredentialProvider implements ServiceCredentialProvider 
   private static Logger LOG = LoggerFactory.getLogger(KuduServiceCredentialProvider.class);
 
   @Override
-  public boolean credentialsRequired(SparkConf sparkConf, Configuration conf) {
+  public boolean credentialsRequired(Configuration conf) {
     return UserGroupInformation.isSecurityEnabled();
   }
 
