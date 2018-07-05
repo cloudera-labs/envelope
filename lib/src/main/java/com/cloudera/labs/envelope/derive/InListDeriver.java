89c0eb02934f0212a0addf2b7db92a3489d338aa
alexvdedov <adedov@cloudera.com>
2018-07-05 12:38:28 -0400

    [ENV-214] Add in-list filter deriver (#201)

---
+++ lib/src/main/java/com/cloudera/labs/envelope/derive/InListDeriver.java
@@ -0,0 +1,215 @@
+/**
+ * Licensed to Cloudera, Inc. under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Cloudera, Inc. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloudera.labs.envelope.derive;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cloudera.labs.envelope.load.ProvidesAlias;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigValueType;
+
+/**
+ * <p>
+ * Returns new dataset by filtering the dependency on a field being in the list
+ * of possible provided values. The list can be either a literal or extracted
+ * from another dependency step.
+ * </p>
+ * <p>
+ * If "dependencies" is a list, a <code>step</code> config parameter is required
+ * to disambiguate operand for the in-list() operation, as is the
+ * <code>field</code> if the <code>step</code>'s schema contians more than one
+ * field.
+ * </p>
+ * <p>
+ * Either <code>values</code> parameter or a combination of
+ * <code>values-step</code>/<code>values-field</code> is required.
+ * </p>
+ */
+
+public class InListDeriver implements Deriver, ProvidesAlias {
+
+  public static final int INLIST_MAX_LIST_SIZE = 1000;
+
+  public static final String INLIST_DERIVER_ALIAS = "in-list";
+  public static final String INLIST_STEP_CONFIG = "step";
+  public static final String INLIST_FIELD_CONFIG = "field";
+  public static final String INLIST_VALUES_CONFIG = "values";
+  public static final String INLIST_REFSTEP_CONFIG = "values-step";
+  public static final String INLIST_REFFIELD_CONFIG = "values-field";
+
+  private enum InListType {
+    LITERAL, REFERENCE
+  }
+
+  private InListType inListType;
+  private Config config;
+  private String stepName;
+  private String fieldName;
+  private String refStepName;
+  private String refFieldName;
+  private List<Object> inList;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(InListDeriver.class);
+
+  @Override
+  public void configure(Config c) {
+    this.config = c;
+    LOGGER.debug("Configure deriver with " + config.toString());
+    if (config.hasPath(INLIST_STEP_CONFIG)) {
+      stepName = config.getString(INLIST_STEP_CONFIG);
+    }
+    if (config.hasPath(INLIST_FIELD_CONFIG)) {
+      fieldName = config.getString(INLIST_FIELD_CONFIG);
+    }
+    if (config.hasPath(INLIST_VALUES_CONFIG)) {
+      if (config.hasPath(INLIST_REFSTEP_CONFIG)) {
+        throw new RuntimeException("Configure: In-List deriver config can't contain both \"" + INLIST_VALUES_CONFIG
+            + "\" and \"" + INLIST_REFSTEP_CONFIG + "\"");
+      } else {
+        if (config.getValue(INLIST_VALUES_CONFIG).valueType() == ConfigValueType.LIST) {
+          inList = config.getList(INLIST_VALUES_CONFIG).unwrapped();
+        } else {
+          throw new RuntimeException("Configure: In-List deriver \"" + INLIST_VALUES_CONFIG + "\" should be a LIST");
+        }
+        inListType = InListType.LITERAL;
+      }
+    } else if (config.hasPath(INLIST_REFSTEP_CONFIG)) {
+      refStepName = config.getString(INLIST_REFSTEP_CONFIG);
+      if (config.hasPath(INLIST_REFFIELD_CONFIG)) {
+        refFieldName = config.getString(INLIST_REFFIELD_CONFIG);
+      }
+      inListType = InListType.REFERENCE;
+    } else {
+      throw new RuntimeException("Configure: In-List deriver config should contain either \"" + INLIST_VALUES_CONFIG
+          + "\" or \"" + INLIST_REFSTEP_CONFIG + "\" parameter");
+    }
+  }
+
+  @Override
+  public Dataset<Row> derive(Map<String, Dataset<Row>> dependencies) throws Exception {
+    LOGGER.debug("Derive: Validating dependencies map " + dependencies.toString());
+    validate(dependencies);
+    String step = getStepName(dependencies);
+    String field = getFieldName(dependencies);
+    Object[] inList = getInList(dependencies);
+    LOGGER.debug("Derive: Filtering dataset " + step + " by field " + field + " being IN "
+        + Arrays.asList(inList).toString() + "");
+    return dependencies.get(step).filter(dependencies.get(step).col(field).isin(inList));
+  }
+
+  @Override
+  public String getAlias() {
+    return INLIST_DERIVER_ALIAS;
+  }
+
+  void validate(Map<String, Dataset<Row>> dependencies) {
+    if (dependencies.size() == 0) {
+      throw new RuntimeException("Validate: In-List deriver requires at least one dependency");
+    } else if (dependencies.size() == 1) {
+      if (stepName != null && !dependencies.containsKey(stepName)) {
+        throw new RuntimeException("Validate: \"" + INLIST_STEP_CONFIG + "\" " + stepName
+            + " is not listed as dependency in " + dependencies.keySet() + "");
+      }
+    } else {
+      if (stepName == null) {
+        throw new RuntimeException("Validate: In-List deriver requires a \"" + INLIST_STEP_CONFIG
+            + "\" configuration when multiple dependencies have been listed: " + dependencies.keySet() + "");
+      }
+      if (!dependencies.containsKey(stepName)) {
+        throw new RuntimeException("Validate: \"" + INLIST_STEP_CONFIG + "\" " + stepName
+            + " is not listed as dependency in " + dependencies.keySet() + "");
+      }
+    }
+    String step = stepName == null ? dependencies.keySet().iterator().next() : stepName;
+    if (dependencies.get(step) == null) {
+      throw new RuntimeException("Validation: the dataset for \"" + INLIST_STEP_CONFIG + "\" " + step + " is NULL");
+    }
+    String[] fields = dependencies.get(step).columns();
+    if (fields.length == 0) {
+      throw new RuntimeException("Validation: the schema for \"" + INLIST_STEP_CONFIG + "\" " + step + " is empty");
+    }
+    if (fieldName == null && fields.length > 1) {
+      throw new RuntimeException(
+          "Validate: \"" + INLIST_FIELD_CONFIG + "\" parameter should be specified to apply the In-List deriver to \""
+              + INLIST_STEP_CONFIG + "\" " + step + "");
+    }
+    if (fieldName != null && !Arrays.asList(fields).contains(fieldName)) {
+      throw new RuntimeException("Validate: the schema for \"" + INLIST_STEP_CONFIG + "\" " + step
+          + " doesn't contain \"" + INLIST_FIELD_CONFIG + "\" " + fieldName + "");
+    }
+
+    if (inListType == InListType.REFERENCE) {
+      if (!dependencies.containsKey(refStepName)) {
+        throw new RuntimeException("Validate: \"" + INLIST_REFSTEP_CONFIG + "\" " + refStepName
+            + " is not listed as dependency in " + dependencies.keySet() + "");
+      }
+      fields = dependencies.get(refStepName).columns();
+      if (fields.length == 0) {
+        throw new RuntimeException(
+            "Validate: the schema for \"" + INLIST_REFSTEP_CONFIG + "\" " + refStepName + " is empty");
+      }
+      if (refFieldName == null && fields.length > 1) {
+        throw new RuntimeException(
+            "Validate: \"" + INLIST_REFFIELD_CONFIG + "\" parameter should be specified to generate in-list from the \""
+                + INLIST_REFSTEP_CONFIG + "\" " + refStepName + "");
+      }
+      if (refFieldName != null && !Arrays.asList(fields).contains(refFieldName)) {
+        throw new RuntimeException("Validate: the schema for \"" + INLIST_REFSTEP_CONFIG + "\" " + refStepName
+            + " doesn't contain \"" + INLIST_REFFIELD_CONFIG + "\" " + refFieldName + "");
+      }
+      String refField = refFieldName == null ? fields[0] : refFieldName;
+      long refListSize = dependencies.get(refStepName).select(refField).distinct().count();
+      if (refListSize > INLIST_MAX_LIST_SIZE) {
+        throw new RuntimeException("Validate: \"" + INLIST_REFFIELD_CONFIG + "\" " + refStepName + "." + refField
+            + " contains " + refListSize + " elements. Maximum allowed size is " + INLIST_MAX_LIST_SIZE + ".");
+      }
+    }
+  }
+
+  private String getStepName(Map<String, Dataset<Row>> dependencies) {
+    return stepName == null ? dependencies.keySet().iterator().next() : stepName;
+  }
+
+  private String getFieldName(Map<String, Dataset<Row>> dependencies) {
+    return fieldName == null ? dependencies.get(getStepName(dependencies)).columns()[0] : fieldName;
+  }
+
+  private String getRefFieldName(Map<String, Dataset<Row>> dependencies) {
+    return refFieldName == null ? dependencies.get(refStepName).columns()[0] : refFieldName;
+  }
+
+  Object[] getInList(Map<String, Dataset<Row>> dependencies) {
+    if (inListType == InListType.REFERENCE) {
+      List<Row> t = dependencies.get(refStepName).select(getRefFieldName(dependencies)).distinct().collectAsList();
+      inList = new ArrayList<Object>(t.size());
+      for (Row r : t) {
+        inList.add(r.get(0));
+      }
+    }
+    return inList.toArray(new Object[0]);
+  }
+
+}
---
+++ lib/src/test/java/com/cloudera/labs/envelope/derive/TestInListDeriver.java
@@ -0,0 +1,340 @@
+/**
+ * Licensed to Cloudera, Inc. under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Cloudera, Inc. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloudera.labs.envelope.derive;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.cloudera.labs.envelope.spark.Contexts;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValueFactory;
+
+/**
+ * In-List Deriver Unit Test
+ */
+public class TestInListDeriver {
+
+  @Test
+  public void testDerive() throws Exception {
+    Dataset<Row> source = createTestDataframe();
+    source.show();
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    dependencies.put("df1", source);
+    dependencies.put("df2", null);
+    dependencies.put("df3", null);
+    List<String> inListLiteral = Arrays.asList(new String[] { "A", "C", "E" });
+    Config config = ConfigFactory.empty()
+        .withValue(InListDeriver.INLIST_STEP_CONFIG, ConfigValueFactory.fromAnyRef("df1"))
+        .withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("id"))
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListLiteral));
+    InListDeriver deriver = new InListDeriver();
+    deriver.configure(config);
+    List<Row> results = deriver.derive(dependencies).select("id").collectAsList();
+    assertEquals(deriver.getInList(dependencies).length, results.size());
+    for (Row row : results) {
+      Object x = row.get(0);
+      assertTrue(inListLiteral.contains(x.toString()));
+    }
+
+    inListLiteral = Arrays.asList(new String[] { "1", "3", "5" });
+    config = config.withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("value"))
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListLiteral));
+    deriver.configure(config);
+    results = deriver.derive(dependencies).select("value").collectAsList();
+    assertEquals(deriver.getInList(dependencies).length, results.size());
+    for (Row row : results) {
+      Object x = row.get(0);
+      assertTrue(inListLiteral.contains(x.toString()));
+    }
+
+    List<Integer> inListIntegers = Arrays
+        .asList(new Integer[] { Integer.valueOf("1"), Integer.valueOf("3"), Integer.valueOf("5") });
+    config = config.withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("value"))
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListIntegers));
+    deriver.configure(config);
+    results = deriver.derive(dependencies).select("value").collectAsList();
+    assertEquals(deriver.getInList(dependencies).length, results.size());
+    for (Row row : results) {
+      Object x = row.get(0);
+      assertTrue(inListIntegers.contains(x));
+    }
+
+    inListLiteral = Arrays.asList(new String[] { "2018-04-01", "2018-04-03", "2018-04-05", "2018-04-26" });
+    config = config.withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("vdate"))
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListLiteral));
+    deriver.configure(config);
+    results = deriver.derive(dependencies).select("vdate").collectAsList();
+    assertEquals(deriver.getInList(dependencies).length, results.size());
+    for (Row row : results) {
+      Object x = row.get(0);
+      assertTrue(inListLiteral.contains(x.toString()));
+    }
+
+    Dataset<Row> ref = createTestDataframe();
+    ref = ref.withColumnRenamed("id", "fk").filter("value < 6");
+    dependencies.put("df2", ref);
+    config = config.withoutPath(InListDeriver.INLIST_VALUES_CONFIG)
+        .withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("id"))
+        .withValue(InListDeriver.INLIST_REFSTEP_CONFIG, ConfigValueFactory.fromAnyRef("df2"))
+        .withValue(InListDeriver.INLIST_REFFIELD_CONFIG, ConfigValueFactory.fromAnyRef("fk"));
+    deriver.configure(config);
+    results = deriver.derive(dependencies).select("id").collectAsList();
+    assertEquals(ref.count(), results.size());
+  }
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  //
+  // According to
+  // https://github.com/lightbend/config/blob/master/config/src/main/java/com/typesafe/config/impl/ConfigImpl.java,
+  // not all Java types can be stored in ConfigList. Method fromAnyRef() only
+  // allows Boolean, "Number", String and Map.
+  //
+  @Test
+  public void testConfigList() throws Exception {
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    List<Date> inListDates = Arrays
+        .asList(new Date[] { new Date(df.parse("2018-04-01").getTime()), new Date(df.parse("2018-04-03").getTime()),
+            new Date(df.parse("2018-04-05").getTime()), new Date(df.parse("2018-04-26").getTime()) });
+    thrown.expect(ConfigException.BugOrBroken.class);
+    thrown.expectMessage("not valid to create ConfigValue");
+    ConfigFactory.empty().withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("vdate"))
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListDates));
+  }
+
+  @Test
+  public void missingConfig() throws Exception {
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("should contain either");
+    Config config = ConfigFactory.empty();
+    new InListDeriver().configure(config);
+  }
+
+  @Test
+  public void ambiguousConfig() throws Exception {
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("can't contain both");
+    List<String> inListLiteral = Arrays.asList(new String[] { "1", "2", "3" });
+    Config config = ConfigFactory.empty()
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListLiteral))
+        .withValue(InListDeriver.INLIST_REFSTEP_CONFIG, ConfigValueFactory.fromAnyRef("df2"));
+    new InListDeriver().configure(config);
+  }
+
+  @Test
+  public void missingField() throws Exception {
+    Dataset<Row> source = createTestDataframe();
+    List<String> inListLiteral = Arrays.asList(new String[] { "1", "2", "3" });
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    dependencies.put("df1", source);
+    Config config = ConfigFactory.empty()
+        .withValue(InListDeriver.INLIST_STEP_CONFIG, ConfigValueFactory.fromAnyRef("df1"))
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListLiteral))
+        .withoutPath(InListDeriver.INLIST_FIELD_CONFIG);
+    Deriver deriver = new InListDeriver();
+    deriver.configure(config);
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("parameter should be specified");
+    deriver.derive(dependencies);
+  }
+
+  @Test
+  public void wrongField() throws Exception {
+    Dataset<Row> source = createTestDataframe();
+    List<String> inListLiteral = Arrays.asList(new String[] { "1", "2", "3" });
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    dependencies.put("df1", source);
+    Config config = ConfigFactory.empty()
+        .withValue(InListDeriver.INLIST_STEP_CONFIG, ConfigValueFactory.fromAnyRef("df1"))
+        .withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("non_existing_field"))
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListLiteral));
+    Deriver deriver = new InListDeriver();
+    deriver.configure(config);
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("doesn't contain \"" + InListDeriver.INLIST_FIELD_CONFIG + "\"");
+    deriver.derive(dependencies);
+  }
+
+  @Test
+  public void missingRefField() throws Exception {
+    Dataset<Row> source = createTestDataframe();
+    Dataset<Row> ref = createTestDataframe();
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    dependencies.put("df1", source);
+    dependencies.put("df2", ref);
+    Config config = ConfigFactory.empty()
+        .withValue(InListDeriver.INLIST_STEP_CONFIG, ConfigValueFactory.fromAnyRef("df1"))
+        .withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("value"))
+        .withValue(InListDeriver.INLIST_REFSTEP_CONFIG, ConfigValueFactory.fromAnyRef("df2"))
+        .withoutPath(InListDeriver.INLIST_REFFIELD_CONFIG);
+    Deriver deriver = new InListDeriver();
+    deriver.configure(config);
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("\"" + InListDeriver.INLIST_REFFIELD_CONFIG + "\" parameter should be specified");
+    deriver.derive(dependencies);
+  }
+
+  @Test
+  public void wrongRefField() throws Exception {
+    Dataset<Row> source = createTestDataframe();
+    Dataset<Row> ref = createTestDataframe();
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    dependencies.put("df1", source);
+    dependencies.put("df2", ref);
+    Config config = ConfigFactory.empty()
+        .withValue(InListDeriver.INLIST_STEP_CONFIG, ConfigValueFactory.fromAnyRef("df1"))
+        .withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("value"))
+        .withValue(InListDeriver.INLIST_REFSTEP_CONFIG, ConfigValueFactory.fromAnyRef("df2"))
+        .withValue(InListDeriver.INLIST_REFFIELD_CONFIG, ConfigValueFactory.fromAnyRef("non_existent_ref_field"));
+    Deriver deriver = new InListDeriver();
+    deriver.configure(config);
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("doesn't contain \"" + InListDeriver.INLIST_REFFIELD_CONFIG + "\"");
+    deriver.derive(dependencies);
+  }
+
+  @Test
+  public void missingDependencies() throws Exception {
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("at least one dependency");
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    List<String> inListLiteral = Arrays.asList(new String[] { "1", "2", "3" });
+    // dependencies.put("df1", null);
+    Config config = ConfigFactory.empty().withValue(InListDeriver.INLIST_VALUES_CONFIG,
+        ConfigValueFactory.fromIterable(inListLiteral));
+    Deriver deriver = new InListDeriver();
+    deriver.configure(config);
+    deriver.derive(dependencies);
+  }
+
+  @Test
+  public void wrongDependencies() throws Exception {
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("not listed as dependency");
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    List<String> inListLiteral = Arrays.asList(new String[] { "1", "2", "3" });
+    dependencies.put("df1", null);
+    dependencies.put("df2", null);
+    Config config = ConfigFactory.empty()
+        .withValue(InListDeriver.INLIST_VALUES_CONFIG, ConfigValueFactory.fromIterable(inListLiteral))
+        .withValue(InListDeriver.INLIST_STEP_CONFIG, ConfigValueFactory.fromAnyRef("df_missing"));
+    Deriver deriver = new InListDeriver();
+    deriver.configure(config);
+    deriver.derive(dependencies);
+
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("not listed as dependency");
+    config.withoutPath(InListDeriver.INLIST_VALUES_CONFIG)
+        .withValue(InListDeriver.INLIST_STEP_CONFIG, ConfigValueFactory.fromAnyRef("df1"))
+        .withValue(InListDeriver.INLIST_REFSTEP_CONFIG, ConfigValueFactory.fromAnyRef("df_missing"));
+    deriver.configure(config);
+    deriver.derive(dependencies);
+  }
+
+  @Test
+  public void ambiguousDependencies() throws Exception {
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("multiple dependencies have been listed");
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    List<String> inListLiteral = Arrays.asList(new String[] { "1", "2", "3" });
+    dependencies.put("df1", null);
+    dependencies.put("df2", null);
+    Config config = ConfigFactory.empty().withValue(InListDeriver.INLIST_VALUES_CONFIG,
+        ConfigValueFactory.fromIterable(inListLiteral));
+    Deriver deriver = new InListDeriver();
+    deriver.configure(config);
+    deriver.derive(dependencies);
+  }
+
+  @Test
+  public void overflowList() throws Exception {
+    Dataset<Row> source = createTestDataframe();
+    StructType refSchema = DataTypes
+        .createStructType(Lists.newArrayList(DataTypes.createStructField("fk", DataTypes.IntegerType, true)));
+    List<Row> refRows = Lists.newArrayList();
+    for (int i = 0; i < InListDeriver.INLIST_MAX_LIST_SIZE + 100; i++) {
+      refRows.add(RowFactory.create(i + 1));
+    }
+    Dataset<Row> ref = Contexts.getSparkSession().createDataFrame(refRows, refSchema);
+
+    Map<String, Dataset<Row>> dependencies = Maps.newHashMap();
+    dependencies.put("df1", source);
+    dependencies.put("df2", ref);
+    Config config = ConfigFactory.empty()
+        .withValue(InListDeriver.INLIST_STEP_CONFIG, ConfigValueFactory.fromAnyRef("df1"))
+        .withValue(InListDeriver.INLIST_FIELD_CONFIG, ConfigValueFactory.fromAnyRef("value"))
+        .withValue(InListDeriver.INLIST_REFSTEP_CONFIG, ConfigValueFactory.fromAnyRef("df2"))
+        .withValue(InListDeriver.INLIST_REFFIELD_CONFIG, ConfigValueFactory.fromAnyRef("fk"));
+    Deriver deriver = new InListDeriver();
+    deriver.configure(config);
+
+    thrown.expect(RuntimeException.class);
+    thrown.expectMessage("Maximum allowed size");
+    deriver.derive(dependencies);
+  }
+
+  private static Dataset<Row> createTestDataframe() throws Exception {
+    StructType schema = createTestSchema();
+    List<Row> rows = createTestData();
+    return Contexts.getSparkSession().createDataFrame(rows, schema);
+  }
+
+  private static StructType createTestSchema() throws Exception {
+    return DataTypes.createStructType(Lists.newArrayList(DataTypes.createStructField("id", DataTypes.StringType, true),
+        DataTypes.createStructField("descr", DataTypes.StringType, true),
+        DataTypes.createStructField("value", DataTypes.IntegerType, true),
+        DataTypes.createStructField("vdate", DataTypes.DateType, true)));
+  }
+
+  private static List<Row> createTestData() throws Exception {
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    return Lists.newArrayList(RowFactory.create("A", "Alfa", 1, new Date(df.parse("2018-04-01").getTime())),
+        RowFactory.create("B", "Bravo", 2, new Date(df.parse("2018-04-02").getTime())),
+        RowFactory.create("C", "Charlie", 3, new Date(df.parse("2018-04-03").getTime())),
+        RowFactory.create("D", "Delta", 4, new Date(df.parse("2018-04-04").getTime())),
+        RowFactory.create("E", "Echo", 5, new Date(df.parse("2018-04-05").getTime())),
+        RowFactory.create("F", "Foxtrot", 6, new Date(df.parse("2018-04-06").getTime())),
+        RowFactory.create("G", "Golf", 7, new Date(df.parse("2018-04-07").getTime())),
+        RowFactory.create("X", "XRay", 24, new Date(df.parse("2018-04-24").getTime())),
+        RowFactory.create("Y", "Yankee", 25, new Date(df.parse("2018-04-25").getTime())),
+        RowFactory.create("Z", "Zulu", 26, new Date(df.parse("2018-04-26").getTime())),
+        RowFactory.create(null, null, null, new Date(df.parse("2018-04-30").getTime())));
+  }
+
+}
